0fff37c222771381d017dcb665a396d2de199beb
Nick Brown <nickbrown714@gmail.com>
2017-02-15 14:06:58 -0800

    print out evaluated config

--- vars/runAssignment.groovy
+++ vars/runAssignment.groovy
@@ -1,11 +1,16 @@
+import static edu.ucsb.cs.anacapa.pipeline.Lib.*
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
+    def cfgstr = jsonString(config)
+
     node {
         stage('Hello World') {
+            sh "echo ${cfgstr}"
             sh 'echo "Hello World!"'
             sh 'env'
         }
--- vars/runGrader.groovy
+++ vars/runGrader.groovy
@@ -1,11 +1,16 @@
+import static edu.ucsb.cs.anacapa.pipeline.Lib.*
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
+    def cfgstr = jsonString(config)
+
     node {
         stage('Hello World') {
+            sh "echo ${cfgstr}"
             sh "echo \"Grading ${config.course_org}/${config.lab_name}-${config.github_user}\""
             sh 'env'
         }
