4c10577dcabe346d755174a8b7e08310c6d685bc
Nick Brown <nickbrown714@gmail.com>
2017-02-15 12:57:01 -0800

    making things more efficient

--- jobs/setupCourse.groovy
+++ jobs/setupCourse.groovy
@@ -2,7 +2,11 @@ folder('Anacapa Grader')
 job('Anacapa Grader/setupCourse') {
   parameters {
     stringParam('course_org', '', 'The course github organization')
-    stringParam('credentials_id', 'github_ncbrown1', 'The identifier of the credentials needed to check out the git repository (note: if using ssh url for git, the credentials will need to be Username + Private Key, otherwise only https is allowed)')
+    credentialsParam('credentials_id') {
+        required()
+        defaultValue('github_ncbrown1')
+        description('The identifier of the credentials needed to check out the git repository (note: if using ssh url for git, the credentials will need to be Username + Private Key, otherwise only https is allowed)')
+    }
   }
   scm {
     github('project-anacapa/anacapa-jenkins-lib')
