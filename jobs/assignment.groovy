3aa37fac61a38cd895ff4fa99d1610ec32579cf7
Nick Brown <nickbrown714@gmail.com>
2017-02-19 15:33:05 -0800

    use env vars to run assignment

--- jobs/assignment.groovy
+++ jobs/assignment.groovy
@@ -24,9 +24,9 @@ pipelineJob("Anacapa Grader/${course_org}/assignment-${lab_name}") {
       node {
         def evars = get_envvars this
         runAssignment {
-          course_org = "${course_org}"
-          credentials_id = "${credentials_id}"
-          lab_name = "${lab_name}"
+          course_org = "${evars['course_org']}"
+          credentials_id = "${evars['credentials_id']}"
+          lab_name = "${evars['lab_name']}"
         }
       }
 	    '''.stripIndent())
