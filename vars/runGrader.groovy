0a724d35ddead9fdd598336a0859036c5073be1b
Nick Brown <nickbrown714@gmail.com>
2017-02-19 15:47:46 -0800

    check out from source

--- vars/runGrader.groovy
+++ vars/runGrader.groovy
@@ -9,10 +9,24 @@ def call(body) {
     def course_org = config.course_org.trim()
     def lab_name = config.lab_name.trim()
     def github_user = config.github_user.trim()
+    def credentials_id = config.credentials_id.trim()
 
     node {
         stage('Hello World') {
             sh "echo \"Grading ${course_org}/${lab_name}-${github_user}\""
+        }
+        stage("Checkout ${course_org}/${lab_name}-${github_user}") {
+          checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/master']],
+            userRemoteConfigs: [
+              [url:"https://github.com/${course_org}/${lab_name}-${github_user}.git"],
+              [credentialsId:"${credentials_id}"]
+            ]
+          ])
+        }
+        stage("Look around") {
+          sh 'ls'
         }
     }
 }
