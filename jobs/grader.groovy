bf067516cb16c72f727c0d86dc7c6612af0984ee
Nick Brown <nickbrown714@gmail.com>
2017-02-19 15:23:32 -0800

    add file context by surrounding runGrader with node

--- jobs/grader.groovy
+++ jobs/grader.groovy
@@ -14,14 +14,16 @@ pipelineJob("Anacapa Grader/${course_org}/grader-${lab_name}") {
     cps {
       script('''
       @Library('anacapa-jenkins-lib') _
-      @Library('anacapa-jenkins-lib') import static edu.ucsb.cs.anacapa.pipeline.Lib.*
+      import static edu.ucsb.cs.anacapa.pipeline.Lib.*
 
-      def evars = get_envvars this
-      runGrader {
-        course_org = "${evars['course_org']}"
-        credentials_id = "${evars['credentials_id']}"
-        lab_name = "${evars['lab_name']}"
-        github_user = "${evars['github_user']}"
+      node {
+        def evars = get_envvars this
+        runGrader {
+          course_org = "${evars['course_org']}"
+          credentials_id = "${evars['credentials_id']}"
+          lab_name = "${evars['lab_name']}"
+          github_user = "${evars['github_user']}"
+        }
       }
 	    '''.stripIndent())
       sandbox()
