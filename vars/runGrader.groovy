590fa5375473ceaea55d0ffc3153bd3ee417c964
Nick Brown <nickbrown714@gmail.com>
2017-02-19 15:57:18 -0800

    adding stash steps from master assignment reference repo

--- vars/runGrader.groovy
+++ vars/runGrader.groovy
@@ -15,7 +15,26 @@ def call(body) {
         stage('Hello World') {
             sh "echo \"Grading ${course_org}/${lab_name}-${github_user}\""
         }
-        stage("Checkout ${course_org}/${lab_name}-${github_user}") {
+        stage("Checkout Assignment Reference Repo") {
+          // start with a clean workspace
+          step([$class: 'WsCleanup'])
+          checkout([
+            $class: 'GitSCM',
+            branches: [[name: '*/master']],
+            userRemoteConfigs: [
+              [url:"https://github.com/${course_org}/assignment-${lab_name}.git"],
+              [credentialsId:"${credentials_id}"]
+            ]
+          ])
+        }
+        stage("Stash reference data") {
+          stash name: "build_data", includes: "resources/build_data/*"
+          stash name: "test_data", includes: "resources/test_data/*"
+          stash name: "expected_outputs", includes: "resources/expected_outputs/*"
+        }
+        stage("Clean WS, Checkout ${course_org}/${lab_name}-${github_user}") {
+          // start with a clean workspace
+          step([$class: 'WsCleanup'])
           checkout([
             $class: 'GitSCM',
             branches: [[name: '*/master']],
@@ -26,7 +45,7 @@ def call(body) {
           ])
         }
         stage("Look around") {
-          sh 'ls'
+          sh 'ls -al'
         }
     }
 }
