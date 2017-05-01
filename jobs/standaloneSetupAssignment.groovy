3a4a13e4c3f266a608b51e4b5380f49a70e3393f
Nick Brown <nickbrown714@gmail.com>
2017-05-01 16:09:56 -0700

    Create standaloneSetupAssignment.groovy

---
+++ jobs/standaloneSetupAssignment.groovy
@@ -0,0 +1,26 @@
+job("AnacapaGrader-setupAssignment") {
+  parameters {
+    stringParam('git_provider_domain', 'github.com', '''
+      The domain of your HTTPS git provider, such as github.com or gitlab.com or
+      github.ucsb.edu.
+    '''.stripIndent())
+    stringParam('course_org', '', '''
+      The name of your course's github organization, github enterprise organization,
+      or gitlab group.
+    '''.stripIndent())
+    credentialsParam('credentials_id') {
+        required()
+        description('The identifier of the credentials needed to check out the git repository (note: if using ssh url for git, the credentials will need to be Username + Private Key, otherwise only https is allowed)')
+    }
+    stringParam('lab_name', '', '''
+      The name of the lab to create (corresponding to an existing git repository
+      with name `assignment-${lab_name}`)
+    '''.stripIndent())
+  }
+  scm {
+    github('project-anacapa/anacapa-jenkins-lib')
+  }
+  steps {
+    dsl(['jobs/assignment.groovy', 'jobs/grader.groovy'], 'IGNORE')
+  }
+}
